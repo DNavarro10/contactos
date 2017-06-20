7639cb0510c2a953a38633e70e1c2b45853d5bb7
DNavarro10 <luis_d_100@hotmail.com>
2017-06-19 23:33:33 -0500

    renombre a vista y agregado php
    Validacion del form usando php

--- index.php
+++ index.php
@@ -1,39 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+	$errores = '';
+	$enviado = '';
 
-<head>
-	<meta charset="UTF-8">
-	<title>Forms</title>
-	<link rel="stylesheet" href="css/Style.css">
-	<link rel="icon" href="img/estudio.ico">
-</head>
-
-<body>
-<div class="contenedor">
-		<div class="titulo">
-			<header>
-				<div><h1>Formulario de contactos</h1></div>
-			</header>
-		</div>
-		<div class="main">
-			<form action="<php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-				<input type="text" class="form-control" id="nombre" name="Nombre" placeholder="Nombre" value="">
-				<input type="text" class="form-control" id="correo" name="Correo" placeholder="Correo" value="">
-				
-				<textarea class="form-control" name="mensaje" id="txt-area" cols="25" rows="10"></textarea>
-					
-				<input type="submit" name="submit" class="btn btn-primary" id="enviar" value="Enviar Correo">
+	if(isset($_POST['submit'])){
+		$nombre = $_POST['nombre'];
+		$correo = $_POST['correo'];
+		$mensaje = $_POST['mensaje'];
 		
-			</form>
-		</div>
-		<footer>
-			<div>
-				<p>
-					Diego Navarro -  2017
-				</p>
-			</div>
-		</footer>
-	</div>
-</body>
+		/* Validar el form*/
+		
+		if(!empty($nombre)){
+			$nombre = trim($nombre); /* Evitar espacios*/
+			$nombre = filter_var($nombre , FILTER_SANITIZE_STRING); /* permine elimnar parametros que no sirven*/
+		} else{
+			$errores .= 'Por favor ingresar un nombre <br />';
+		}
+		if(!empty($correo)){
+			$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+			
+			if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+				$errores .= 'Por favor ingresa un correo valido <br />';
+			}
+		} else {
+		 	$errores .= 'Por favor ingres el correo';
+		}
+	}
 
-</html>
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Forms</title>
+	<link rel="stylesheet" href="css/Style.css">
+	<link rel="icon" href="img/estudio.ico">
+</head>
+
+<body>
+<div class="contenedor">
+		<div class="titulo">
+			<header>
+				<div><h1>Formulario de contactos</h1></div>
+			</header>
+		</div>
+		<div class="main">
+			<form action="<php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+				<input type="text" class="form-control" id="nombre" name="Nombre" placeholder="Nombre" value="">
+				<input type="text" class="form-control" id="correo" name="Correo" placeholder="Correo" value="">
+				
+				<textarea class="form-control" name="mensaje" id="txt-area" cols="25" rows="10"></textarea>
+			 	 <!-- validado php -->
+					<?php if(!empty($_errores)): ?>
+						<div class="alert error">
+							<?php echo $errores; ?>
+						</div>
+					<?php elseif($enviado): ?>
+						<div class="alert success">
+							<p>Enviado Correctamente</p>
+						</div>
+					<?php endif ?>
+				
+				<input type="submit" name="submit" class="btn btn-primary" id="enviar" value="Enviar Correo">
+		
+			</form>
+		</div>
+		<footer>
+			<div>
+				<p>
+					Diego Navarro -  2017
+				</p>
+			</div>
+		</footer>
+	</div>
+</body>
+
+</html>
